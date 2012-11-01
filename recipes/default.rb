c2bfa07916512e774363f7df0e2d5a53604b959e
Dan Brubaker Horst <dbrubak1@nd.edu>
2012-11-01 14:45:38 -0400

    Parameterize recipe; add service account

--- recipes/default.rb
+++ recipes/default.rb
@@ -1,12 +1,28 @@
 #
-# Cookbook Name:: hydra-jetty
+# Cookbook Name:: hydra_jetty
 # Recipe:: default
 #
 # The contents of this file have been relased under the Apache 2 license.
 #
 
-directory "/var/hydra-jetty/" do
-  owner 'root'
-  group 'root'
+user node[:hydra_jetty][:user] do
   action :create
+  system true
+  shell '/bin/false'
+end
+
+group node[:hydra_jetty][:group] do
+  append node[:hydra_jetty][:group]
+end
+
+directory node[:hydra_jetty][:dir]  do
+  owner node[:hydra_jetty][:user]
+  group node[:hydra_jetty][:group]
+  action :create_if_missing
+end
+
+git node[:hydra_jetty][:dir] do
+  repository node[:hydra_jetty][:git_repo]
+  reference node[:hydra_jetty][:git_ref]
+  action :sync
 end
