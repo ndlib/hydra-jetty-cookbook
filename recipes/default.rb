ef60863f637969cd8df634309ba91e3dd37ff64d
Dan Brubaker Horst <dbrubak1@nd.edu>
2012-11-01 15:10:17 -0400

    Simplifying recipe

--- recipes/default.rb
+++ recipes/default.rb
@@ -5,24 +5,19 @@
 # The contents of this file have been relased under the Apache 2 license.
 #
 
+group node[:hydra_jetty][:group]
+
 user node[:hydra_jetty][:user] do
+  group node[:hydra_jetty][:group]
   action :create
   system true
   shell '/bin/false'
 end
 
-group node[:hydra_jetty][:group] do
-  append node[:hydra_jetty][:group]
-end
-
-directory node[:hydra_jetty][:dir]  do
-  owner node[:hydra_jetty][:user]
-  group node[:hydra_jetty][:group]
-  action :create_if_missing
-end
-
 git node[:hydra_jetty][:dir] do
   repository node[:hydra_jetty][:git_repo]
   reference node[:hydra_jetty][:git_ref]
+  user node[:hydra_jetty][:user]
+  group node[:hydra_jetty][:group]
   action :sync
 end
