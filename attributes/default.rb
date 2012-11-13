1e88ff27150081ee34308f307fbca5df7dec6fa8
Dan Brubaker Horst <dbrubak1@nd.edu>
2012-11-13 09:11:31 -0500

    Work on jetty daemon

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,7 +1,14 @@
-default[:hydra_jetty][:git_repo]   = 'git://github.com/projecthydra/hydra-jetty.git'
-default[:hydra_jetty][:git_ref]    = 'master'
-default[:hydra_jetty][:user]       = 'jetty'
-default[:hydra_jetty][:group]      = 'jetty'
-default[:hydra_jetty][:dir]        = '/var/jetty'
-default[:hydra_jetty][:java_opts]  = '-XX:+CMSPermGenSweepingEnabled -XX:+CMSClassUnloadingEnabled -XX:PermSize=64M -XX:MaxPermSize=128M'
-default[:hydra_jetty][:start_file] = 'start.jar'
+default[:hydra_jetty][:git_repo]     = 'git://github.com/projecthydra/hydra-jetty.git'
+default[:hydra_jetty][:git_ref]      = 'master'
+default[:hydra_jetty][:user]         = 'jetty'
+default[:hydra_jetty][:group]        = 'jetty'
+default[:hydra_jetty][:dir]          = '/var/jetty'
+default[:hydra_jetty][:log]          = '/var/log/jetty.log'
+default[:hydra_jetty][:start_file]   = 'start.jar'
+default[:hydra_jetty][:java]         = '/usr/bin/java'
+default[:hydra_jetty][:java_options] = [
+  '-XX:+CMSPermGenSweepingEnabled',
+  '-XX:+CMSClassUnloadingEnabled',
+  '-XX:PermSize=64M',
+  '-XX:MaxPermSize=128M'
+]
--- recipes/default.rb
+++ recipes/default.rb
@@ -5,25 +5,63 @@
 # The contents of this file have been relased under the Apache 2 license.
 #
 
+# Create service account
+
 group node[:hydra_jetty][:group]
 
 user node[:hydra_jetty][:user] do
-  group node[:hydra_jetty][:group]
   action :create
+  group  node[:hydra_jetty][:group]
+  home   node[:hydra_jetty][:dir]
+  shell  '/bin/bash'
   system true
-  shell '/bin/false'
 end
 
+# Install software
+
 directory node[:hydra_jetty][:dir] do
-  owner node[:hydra_jetty][:user]
-  group node[:hydra_jetty][:group]
-  action :create
+  action    :create
+  group     node[:hydra_jetty][:group]
+  owner     node[:hydra_jetty][:user]
+  recursive true
 end
 
 git node[:hydra_jetty][:dir] do
+  action     :sync
+  group      node[:hydra_jetty][:group]
+  reference  node[:hydra_jetty][:git_ref]
   repository node[:hydra_jetty][:git_repo]
-  reference node[:hydra_jetty][:git_ref]
-  user node[:hydra_jetty][:user]
-  group node[:hydra_jetty][:group]
-  action :sync
+  user       node[:hydra_jetty][:user]
+end
+
+file node[:hydra_jetty][:log] do
+  action :create_if_missing
+  group  node[:hydra_jetty][:group]
+  owner  node[:hydra_jetty][:user]
+end
+
+# Configure service
+
+directory '/var/chef/jetty/bin' do
+  action    :create
+  recursive true
+end
+
+template '/var/chef/jetty/bin/start' do
+  mode   '0751'
+  source 'jetty/start.erb'
+end
+
+template '/var/chef/jetty/bin/stop' do
+  mode   '0751'
+  source 'jetty/stop.erb'
+end
+
+template '/etc/init.d/jettyd' do
+  mode   '0751'
+  source 'jettyd.erb'
+end
+
+service 'jettyd' do
+  action [ :enable, :start ]
 end
